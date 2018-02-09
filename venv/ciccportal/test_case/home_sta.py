ef46abed119c18ac3c02bffc22ee93d5244454dc
zhhan <zhhan@niub.la>
2018-02-09 12:01:02 +0800

    finish home_page

--- venv/ciccportal/test_case/home_sta.py
+++ venv/ciccportal/test_case/home_sta.py
@@ -1,12 +1,11 @@
 import unittest, random, sys
-from selenium.webdriver.common.by import By
 sys.path.append("./models")
 sys.path.append("./page_obj")
 sys.path.append("./login_sta")
 from models import myunit, function
 from page_obj.homePage import home
 from page_obj.loginPage import login
-from page_obj.base import Page
+
 
 class homeTest(myunit.MyTest):
     '''中金研报首页测试'''
@@ -21,6 +20,46 @@ class homeTest(myunit.MyTest):
         po.daily_recommendations()
         self.assertIn('每日推荐', po.page_title())
         function.insert_img(self.driver, "daily_recommendations.png")
+
+    def test_history_recommended(self):
+        '''进入每日推荐页面'''
+        self.user_login_verify(username='manager@cicc.com', password='111111')
+        po = home(self.driver)
+        po.history_recommended()
+        self.assertIn('历史推荐', po.page_title())
+        function.insert_img(self.driver, "history_recommended.png")
+
+    def test_morning_focus(self):
+        '''进入晨会焦点页面'''
+        self.user_login_verify(username='manager@cicc.com', password='111111')
+        po = home(self.driver)
+        po.morning_focus()
+        self.assertIn('晨会焦点', po.page_title())
+        function.insert_img(self.driver, "morning_focus.png")
+
+    def test_exclusive_recommended(self):
+        '''进入专属推荐页面'''
+        self.user_login_verify(username='manager@cicc.com', password='111111')
+        po = home(self.driver)
+        po.exclusive_recommended()
+        self.assertIn('研报详情', po.page_title())
+        function.insert_img(self.driver, "exclusive_recommended.png")
+
+    def test_popular_profession(self):
+        '''进入热门行业页面'''
+        self.user_login_verify(username='manager@cicc.com', password='111111')
+        po = home(self.driver)
+        po.popular_profession()
+        self.assertIn('研报详情', po.page_title())
+        function.insert_img(self.driver, "popular_profession.png")
+
+    def test_hot_company(self):
+        '''进入热门公司页面'''
+        self.user_login_verify(username='manager@cicc.com', password='111111')
+        po = home(self.driver)
+        po.hot_company()
+        self.assertIn('研报详情', po.page_title())
+        function.insert_img(self.driver, "hot_company.png")
 
 if __name__ == '__main__':
     unittest.main()
--- venv/ciccportal/test_case/page_obj/homePage.py
+++ venv/ciccportal/test_case/page_obj/homePage.py
@@ -7,9 +7,35 @@ class home(Page):
     '''
     中金研报首页
     '''
-    daily_recommendations_loc = (By.CSS_SELECTOR,'html body div.container-fluid div.col-xs-9.w910 div.c-panel-list div.recommended_daily div.c-panel-box div div.panel-info div')
+    #每日推荐
+    daily_recommendations_loc = (By.CSS_SELECTOR, 'body > div.container-fluid > div.col-xs-9.w910 > div.c-panel-list > div > div:nth-child(1) > div:nth-child(2) > div > div:nth-child(1) > h4 > a')
     def daily_recommendations(self):
         self.find_element(*self.daily_recommendations_loc).click()
+
+    #历史推荐
+    history_recommended_loc = (By.CSS_SELECTOR, 'body > div.container-fluid > div.col-xs-9.w910 > div.c-panel-title > div > a')
+    def history_recommended(self):
+        self.find_element(*self.history_recommended_loc).click()
+
+    #晨会焦点
+    morning_focus_loc = (By.CSS_SELECTOR, '#latestDocument')
+    def morning_focus(self):
+        self.find_element(*self.morning_focus_loc).click()
+
+    #专属推荐
+    exclusive_recommended_loc = (By.CSS_SELECTOR, 'body > div.container-fluid > div.pull-right.w270 > div.c-recommend > div.list-shadow > div:nth-child(1) > a > p')
+    def exclusive_recommended(self):
+        self.find_element(*self.exclusive_recommended_loc).click()
+
+    #热门行业
+    popular_profession_loc = (By.CSS_SELECTOR, '#hy-jr > div:nth-child(1) > a > p')
+    def popular_profession(self):
+        self.find_element(*self.popular_profession_loc).click()
+
+    #热门公司
+    hot_company_loc = (By.CSS_SELECTOR, '#gs-jr > div:nth-child(1) > a > p')
+    def hot_company(self):
+        self.find_element(*self.hot_company_loc).click()
 
     #验证页面进入成功
     def page_title(self):
